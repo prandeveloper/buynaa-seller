d4880fe16717fb482cd3b8897aec35eef41e9369
prandeveloper <pranaydevelopersveltosest@gmail.com>
2022-03-08 07:35:06 +0530

    morning

---
+++ build/static/js/1.bb96661f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[1],{825:function(a,s,d){}}]);
+//# sourceMappingURL=1.bb96661f.chunk.js.map
